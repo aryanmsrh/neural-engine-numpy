333e09a0b9bb33551e995fa911267726f9d1515d
Aryan Mishra <aryanmsrh@gmail.com>
2026-09-10 16:45:43 +0530

    Clean up

--- nn/init.py
+++
@@ -1,6 +0,0 @@
-# module exports for nn library
-from .layers import Dense, ReLU, SoftmaxCrossEntropy
-from .optimizers import SGD
-from .models import Sequential
-
-__all__ = ["Dense", "ReLU", "SoftmaxCrossEntropy", "SGD", "Sequential"]
